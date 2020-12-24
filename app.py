370e16b83f47d651273effe7f6b3c38fd337fe6b
shri678 <71198310+shri678@users.noreply.github.com>
2020-12-23 22:26:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div([
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Tab one', value='tab-1'),
